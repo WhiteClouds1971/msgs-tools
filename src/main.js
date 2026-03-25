293490a2a3bf217cd19ba521c3a3f0926ca0a35d
whiteclouds <whiteclouds1971@163.com>
2026-03-25 16:29:12 +0800

    refactor: 移除vant插件及相关依赖
    删除vant.js插件文件并移除main.js中的引用，同时清理package.json中未使用的vite-plugin-imagemin依赖

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import '@/assets/iconfont/iconfont.css';
 import '@/assets/css/index.css';
 import 'vant/lib/index.css';
 import { marked } from '@/plugins/marked.js';
-import { vant } from '@/plugins/vant.js';
 import { pinia } from '@/stores/index.js';
 
-createApp(App).use(vant).use(router).use(pinia).use(marked).mount('#app');
+createApp(App).use(router).use(pinia).use(marked).mount('#app');
